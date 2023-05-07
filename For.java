b11a51119efa8d6b3a95b99926f8b16a901deb54
Jose Manuel Jaita Chura <jjaitac@unsa.edu.pe>
2023-05-07 12:53:38 -0500

    comentario

--- For.java
+++ For.java
@@ -1,10 +1,11 @@
-public class For{
+public class For {
     public static void main(String[] args) {
-        String[] cars = {"Volvo","BMW","Ford","Mazda"};
+        String[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
         for (String i : cars) {
             System.out.println(i);
-            
-        } 
-        
+            // System.out.println(i);
+
+        }
+
     }
 }
